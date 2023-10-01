aae0e36eea33989e32aa89fb6335b9818503e8be
Ridouane18 <redtansouft@gmail.com>
2023-10-01 13:15:32 +0100

    added icons as SVG because its expensive ro add a library for 2 icons

--- components/SocialMediaFooer/SocialMediaFooer.tsx
+++ components/SocialMediaFooer/SocialMediaFooer.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import styles from './SocialMediaFooer.module.css';
+import github from 'assets/icons8-github.svg';
+import linkedIn from 'assets/icons8-linkedin.svg';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const SocialMediaFooer = () => {
   return (
     <div className={styles.container}>
-        <p>F   </p>
-        <p>L   </p>
-        <p>GIT</p>
+        <Link href={'https://www.linkedin.com/in/ridouane-tansouft-952868150/'} target="_blank">
+            <Image src={linkedIn} width={40} height={40} alt={'linkedIn'}/>
+        </Link>
+        <Link href={'https://github.com/Ridouane18'} target="_blank">
+             <Image src={github} width={40} height={40} alt={'github'}/>
+        </Link>
     </div>
   )
 }
